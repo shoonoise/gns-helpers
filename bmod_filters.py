ebf168d7eba8efe257206d136ac2fdcc6dc4acd4
Devaev Maxim <mdevaev@gmail.com>
2014-02-11 19:25:13 +0400

    bmod_filters refactoring

--- bmod_filters.py
+++ bmod_filters.py
@@ -1,18 +1,22 @@
 from raava import rules
 
 
+##### Private classes #####
+class _Cmp:
+    eq = rules.EqComparator # XXX: eq: key=value
+    ne = rules.NeComparator
+    ge = rules.GeComparator
+    gt = rules.GtComparator
+    le = rules.LeComparator
+    lt = rules.LtComparator
+    in_list = rules.InListComparator
+    not_in_list = rules.NotInListComparator
+    regexp = rules.RegexpComparator
+
+
 ##### Public constants #####
 BUILTINS_MAP = {
-    "eq":          rules.EqComparator, # XXX: eq: key=value
-    "ne":          rules.NeComparator,
-    "ge":          rules.GeComparator,
-    "gt":          rules.GtComparator,
-    "le":          rules.LeComparator,
-    "lt":          rules.LtComparator,
-    "in_list":     rules.InListComparator,
-    "not_in_list": rules.NotInListComparator,
-    "regexp":      rules.RegexpComparator,
-
+    "cmp":             _Cmp,
     "match_event":     rules.match_event,
     "match_extra":     rules.match_extra,
     "disable_handler": rules.disable_handler,
