260db0286718e42300f3bda992346c4c7bfb2576
Devaev Maxim <mdevaev@gmail.com>
2014-02-11 19:24:46 +0400

    Fixed plugins import

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
+import os
 from .. import plugins
 
 
 ##### Public methods #####
 def load_builtins(config_dict):
-    return plugins.load_plugins(config_dict, "gns.bltins.", "bmod_", "BUILTINS_MAP")
+    return plugins.load_plugins(config_dict, os.path.dirname(__file__), "gns.bltins.", "bmod_", "BUILTINS_MAP")
 
