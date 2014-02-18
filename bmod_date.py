7ed0ac67960f63118743dfb6f4597d1d9df635bd
Devaev Maxim <mdevaev@gmail.com>
2014-02-18 21:23:01 +0400

    Refactoring

--- bmod_date.py
+++ bmod_date.py
@@ -4,35 +4,35 @@ import dateutil.parser
 
 ##### Private classes #####
 class _Date:
-    def __init__(self, timestr = None):
+    def __init__(self, timestr):
         if timestr is not None:
-            self._datetime = dateutil.parser.parse(timestr)
+            self.datetime = dateutil.parser.parse(timestr)
         else:
-            self._datetime = datetime.datetime.now()
+            self.datetime = datetime.datetime.now()
 
     @staticmethod
-    def now(timestr):
-        return _Date(timestr)
+    def now():
+        return _Date(None)
 
-    def get_datetime(self):
-        return self._datetime
+    @property
+    def weekday(self):
+        return self.datetime.weekday()
 
-    def is_workday(self = None):
-        return ( (self or _Date()).get_datetime().weekday() in tuple(range(0, 5)) )
+    @property
+    def month(self):
+        return self.datetime.month
 
-    def is_offday(self = None):
-        return ( (self or _Date()).get_datetime().weekday() in tuple(range(5, 7)) )
+    def is_workday(self):
+        return ( self.weekday in tuple(range(0, 5)) )
 
-    def get_day(self = None):
-        return (self or _Date()).get_datetime().weekday()
-
-    def get_month(self = None):
-        return (self or _Date()).get_datetime().month
+    def is_offday(self):
+        return ( self.weekday in tuple(range(5, 7)) )
 
 for (name, value) in zip(
         ("jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec") +
-        ("mon", "tue", "wed", "thu", "fri", "sat", "sun"),
-        tuple(range(1, 13)) + tuple(range(7)),
+        ("mon", "tue", "wed", "thu", "fri", "sat", "sun") +
+        ("workdays", "offdays"),
+        tuple(range(1, 13)) + tuple(range(7)) + (tuple(range(0, 5)), tuple(range(5, 7))),
     ):
     setattr(_Date, name, value)
 
