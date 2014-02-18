4d3acf82da855807562f77d596f4f14e48c3d11e
Devaev Maxim <mdevaev@gmail.com>
2014-02-18 21:37:37 +0400

    Refactoring #2

--- bmod_date.py
+++ bmod_date.py
@@ -4,6 +4,9 @@ import dateutil.parser
 
 ##### Private classes #####
 class _Date:
+    workdays = tuple(range(0, 5))
+    offdays = tuple(range(5, 7))
+
     def __init__(self, timestr):
         if timestr is not None:
             self.datetime = dateutil.parser.parse(timestr)
@@ -22,17 +25,10 @@ class _Date:
     def month(self):
         return self.datetime.month
 
-    def is_workday(self):
-        return ( self.weekday in tuple(range(0, 5)) )
-
-    def is_offday(self):
-        return ( self.weekday in tuple(range(5, 7)) )
-
 for (name, value) in zip(
         ("jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec") +
-        ("mon", "tue", "wed", "thu", "fri", "sat", "sun") +
-        ("workdays", "offdays"),
-        tuple(range(1, 13)) + tuple(range(7)) + (tuple(range(0, 5)), tuple(range(5, 7))),
+        ("mon", "tue", "wed", "thu", "fri", "sat", "sun"),
+        tuple(range(1, 13)) + tuple(range(7)),
     ):
     setattr(_Date, name, value)
 
