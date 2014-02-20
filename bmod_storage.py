d992f309f54baccc416f4fdc5a90ce859a0fa1ef
Devaev Maxim <mdevaev@gmail.com>
2014-02-20 19:10:23 +0400

    bmod_storage: added storage.lock() removed checkpoints from storage.get()/set()

--- bmod_storage.py
+++ bmod_storage.py
@@ -2,7 +2,6 @@ import pickle
 import logging
 
 from raava import zoo
-from raava import worker
 
 from .. import service
 from .. import env
@@ -26,11 +25,15 @@ class _NoDefault:
 
 
 ##### Private methods #####
-def _client_context():
-    return zoo.Connect(env.get_config(service.S_CORE, service.O_ZOO_NODES))
+def _client_opts():
+    return env.get_config(service.S_CORE, service.O_ZOO_NODES)
 
-def _set_value(task, path, value):
-    task.checkpoint()
+def _client_context():
+    return zoo.Connect(_client_opts())
+
+
+###
+def _set_value(path, value):
     data = pickle.dumps(value)
     path = zoo.join(_USER_PATH, path)
     with _client_context() as client:
@@ -38,30 +41,46 @@ def _set_value(task, path, value):
             client.create(path, data, makepath=True)
         except zoo.NodeExistsError:
             client.set(path, data)
-    del client
-    task.checkpoint()
 
-def _get_value(task, path, default=_NoDefault):
-    task.checkpoint()
+def _get_value(path, default=_NoDefault):
     path = zoo.join(_USER_PATH, path)
     with _client_context() as client:
         try:
             result = client.pget(path)
-        except zoo.NoNodeError:
+        except (zoo.NoNodeError, EOFError):
             if default is not _NoDefault:
                 result = default
             else:
                 _logger.exception("Non-existent node: %s", path)
                 raise
-    del client
-    task.checkpoint()
     return result
+
+
+##### Private classes ####
+class _Lock:
+    def __init__(self, path):
+        self._path = path
+        self._client = None
+        self._lock = None
+
+    def __enter__(self):
+        self._client = zoo.connect(_client_opts())
+        self._lock = self._client.Lock(zoo.join(_USER_PATH, self._path, "lock"))
+        self._lock.acquire()
+
+    def __exit__(self, type, value, traceback): # pylint: disable=W0622
+        self._lock.release()
+        self._lock = None
+        self._client.stop()
+        self._client = None
+
 
 
 ##### Private classes #####
 class _Storage:
-    set = worker.make_task_builtin(_set_value)
-    get = worker.make_task_builtin(_get_value)
+    set = _set_value
+    get = _get_value
+    lock = _Lock
 
 
 ##### Public constants #####
