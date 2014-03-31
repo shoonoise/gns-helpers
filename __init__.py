85d4067f11d98a774422b48212580e1717b19aef
Devaev Maxim <mdevaev@gmail.com>
2014-03-31 16:36:48 +0400

    Removed old __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-import os
-from .. import plugins
-
-
-##### Public methods #####
-def load_builtins(config_dict):
-    return plugins.load_plugins(config_dict, os.path.dirname(__file__), "gns.bltins.", "bmod_", "BUILTINS_MAP")
-
