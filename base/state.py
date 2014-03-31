89852cfb237a9367f23bf5425504fd15325f545e
Devaev Maxim <mdevaev@gmail.com>
2014-03-31 16:14:54 +0400

    base.state no longer builtin

--- base/state.py
+++ base/state.py
@@ -1,15 +1,12 @@
-# required:
-#   - bmod_const
-#   - bmod_storage
-
-
-import builtins
 import logging
 
 from ulib import typetools
 
 from raava import zoo
 from raava import rules
+
+from . import const
+from . import storage
 
 
 ##### Private objects #####
@@ -19,15 +16,15 @@ _logger = logging.getLogger(__name__)
 ##### Private methods #####
 def _add_previous(*fields):
     fields = sorted( fields or (
-            builtins.EVENT.HOST, # pylint: disable=E1101
-            builtins.EVENT.SERVICE, # pylint: disable=E1101
+            const.EVENT.HOST,
+            const.EVENT.SERVICE,
         ))
     def make_method(method):
         def wrap(event_root, **kwargs):
             check_id = typetools.object_hash([ event_root.get(field) for field in fields ])
             check_path = zoo.join("_state", check_id)
             version = event_root.get_extra()[rules.EXTRA.COUNTER]
-            (kwargs["previous"], old_version, write_ok) = builtins.storage.replace( # pylint: disable=E1101
+            (kwargs["previous"], old_version, write_ok) = storage.replace(
                 path=check_path,
                 value=event_root,
                 default=None,
@@ -45,7 +42,7 @@ def _is_changed(previous, current, from_, to, field=None):
     if previous is None:
         return True
     if field is None:
-        field = builtins.EVENT.STATUS # pylint: disable=E1101
+        field = EVENT.STATUS
     is_changed = (
         (from_ is None or from_ == previous.get(field)) and
         (to is None or to == current.get(field)) and
@@ -55,13 +52,13 @@ def _is_changed(previous, current, from_, to, field=None):
 
 
 ##### Private classes #####
-class _State:
+class state:
     add_previous = _add_previous
     is_changed = _is_changed
 
 
-##### Public constants #####
-BUILTINS_MAP = {
-    "state": _State,
-}
+##### Provides #####
+__all__ = (
+    "state",
+)
 
