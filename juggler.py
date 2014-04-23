65cc4dd13aed3f994ad450b8e3666ac39171140f
Devaev Maxim <mdevaev@gmail.com>
2014-04-23 19:00:36 +0400

    juggler: added task.checkpoint()

--- juggler.py
+++ juggler.py
@@ -1,13 +1,15 @@
 import requests
 import logging
 
+from raava import worker
+
 
 ##### Private objects #####
 _logger = logging.getLogger(__name__)
 
 
 ##### Private methods #####
-def send_raw(to, host, service, status, description, instance=""):
+def _send_raw(task, to, host, service, status, description, instance=""):
     _logger.debug("Sending to Juggler: %s", to)
     ok = False
     try:
@@ -25,4 +27,9 @@ def send_raw(to, host, service, status, description, instance=""):
             _logger.error("Failed to send SMS to %s: %d %s; %s", to, result.status_code, result.reason, result.text)
     except Exception:
         _logger.exception("Failed to send SMS to %s", to)
+    task.checkpoint()
     return ok
+
+
+##### Public methods #####
+send_raw = worker.make_task_method(_send_raw) # pylint: disable=C0103
