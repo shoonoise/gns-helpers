f9c3aefd7467c2f093f514ecd73a98860c40763a
Devaev Maxim <mdevaev@gmail.com>
2014-04-23 18:32:50 +0400

    gitrules: removed fmod_git dependency

--- gitrules.py
+++ gitrules.py
@@ -1,9 +1,14 @@
 import os
 import re
+import subprocess
+import logging
 
 from gns import env
 from gns import service
-from gns.fetchers import fmod_git
+
+
+##### Private objects #####
+_logger = logging.getLogger(__name__)
 
 
 ##### Public methods #####
@@ -15,13 +20,26 @@ def get_head_path():
     return git_path
 
 def get_local_head():
-    version = fmod_git._shell_exec("git rev-parse HEAD", cwd=get_head_path()).strip() # pylint: disable=W0212
+    version = _shell_exec("git rev-parse HEAD", cwd=get_head_path()).strip()
     return version
 
 def get_remote_head():
-    repo_url = env.get_config(fmod_git.S_GIT, fmod_git.O_REPO_URL)
-    proc_output = fmod_git._shell_exec("git ls-remote {} HEAD".format(repo_url)) # pylint: disable=W0212
+    repo_url = env.get_config("git", "repo-url")
+    proc_output = _shell_exec("git ls-remote {} HEAD".format(repo_url))
     version_match = re.match(r"^([0-9a-fA-F]{40})[ \t]+HEAD$", proc_output.strip())
     assert version_match is not None, "Unexpected git-ls-remote output:\n{}".format(proc_output)
     version = version_match.group(0)
     return version
+
+
+##### Private methods #####
+def _shell_exec(command, cwd=None):
+    proc_stdout = subprocess.check_output(
+        command,
+        env={ "LC_ALL": "C" },
+        universal_newlines=True,
+        shell=True,
+        cwd=cwd,
+    )
+    _logger.debug("Command '{}' stdout:\n{}".format(command, proc_stdout))
+    return proc_stdout
